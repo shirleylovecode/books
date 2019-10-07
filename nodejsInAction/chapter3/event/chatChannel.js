c8a5725641741ed910b72b69ac6ad06d3b32a367
杨超凡 <chaofan0926@163.com>
2019-10-07 20:20:53 +0800

    event

--- nodejsInAction/chapter3/event/chatChannel.js
+++ nodejsInAction/chapter3/event/chatChannel.js
@@ -34,25 +34,53 @@ channel.clients = {};
 channel.subscriptions = {};
 
 channel.on('join', function(client, id) {
-  console.log(this);
+  const welcome = `Welcome! There are ${this.listeners('broadcast').length} guests online.`;
+  client.write(welcome);
   this.clients[id] = client;
   this.subscriptions[id] = (senderId, message) => {
-    console.log(this);
-    if(senderId !== id) {
+    if(!senderId || (senderId !== id  && this.clients [senderId])) {
       this.clients[id].write(message);
     }
   }
 
-  channel.on('broadcast', function(senderId, message){
-    this.subscriptions[id](senderId, message)
-  })
+  
+
+  
+
+  //channel上对于broadcast监听器注册了多个回调函数，如果想要remove的话，一定要正确指定回调函数.
+  channel.on('broadcast', this.subscriptions[id]);
 });
+
+channel.on('shutdown', function () {
+  this.emit('broadcast', '', 'Server is shutting down');
+
+  this.removeAllListeners('broadcast');
+  this.clients = {};
+  this.subscriptions = {};
+})
+
+channel.on('disconnect', function (id) {
+  channel.removeListener('broadcast', this.subscriptions[id]);
+  delete this.subscriptions[id];
+  delete this.clients[id];
+
+  this.emit('broadcast', '', `${id} has left Chat`);
+})
 
 const server = net.createServer((client) => {
   const id = `${client.remoteAddress}:${client.remotePort}`;
   channel.emit('join', client, id);
 
   client.on('data', (data) => {
-    channel.emit('broadcast', id, data.toString());
+    if(data.toString() === 'shutdown\r\n') {
+      channel.emit('shutdown');
+    } else {
+      channel.emit('broadcast', id, data.toString());
+    }
+  });
+
+  client.on('close', function() {
+    channel.emit('disconnect', id);
   })
+
 }).listen(3000);
